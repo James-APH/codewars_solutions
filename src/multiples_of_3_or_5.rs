dfb0ca08d76c095402d13dd3536ceae1068daa64
James Huston <jamzhuston@gmail.com>
2025-07-19 12:49:39 -0600

    finished m35

--- src/multiples_of_3_or_5.rs
+++ src/multiples_of_3_or_5.rs
@@ -1,8 +1,20 @@
-pub fn solution(num: i32) -> i32 {
-    (0..num)
-        .collect::<Vec<i32>>()
-        .into_iter()
-        .filter(|x| x % 3 == 0 || x % 5 == 0)
-        .reduce(|acc, e| acc + e)
-        .unwrap()
+/// First Solution
+/*
+    fn solution(num: i32) -> i32 {
+        if num <= 0 {
+            0
+        } else {
+            (0..num)
+                .collect::<Vec<i32>>()
+                .into_iter()
+                .filter(|x| x % 3 == 0 || x % 5 == 0)
+                .reduce(|acc, e| acc + e)
+                .unwrap()
+        }
+    }
+*/
+/// Second solution, did not realize there was a sum func
+
+fn solution(num: i32) -> i32 {
+    (0..num).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
 }
