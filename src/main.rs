3360929a77e288ef8d811e48d33330669f51727f
James Huston <jamzhuston@gmail.com>
2025-07-20 00:20:39 -0600

    start up files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod disemvowel_trolls;
 mod multiples_of_3_or_5;
+mod vowel_count;
+mod your_order_please;
 
 fn main() {
     println!("Hello, world!");
+    println!("{}", your_order_please::order("one1 three3 four4 two2"));
 }
