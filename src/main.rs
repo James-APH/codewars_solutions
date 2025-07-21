0b36ed90f7310d8d8d8cb791d449ae3d6a87514e
James Huston <jamzhuston@gmail.com>
2025-07-21 12:36:33 -0600

    have been adding solutions as modules so nvim will let me use rust-analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod disemvowel_trolls;
+mod greed_is_good;
 mod multiples_of_3_or_5;
+mod persistent_bugger;
+mod row_weights;
+mod sort_numbers;
+mod take_a_ten_minutes_walk;
+mod two_to_one;
 mod vowel_count;
+mod who_likes_it;
 mod your_order_please;
-
 fn main() {
     println!("Hello, world!");
-    println!("{}", your_order_please::order("one1 three3 four4 two2"));
 }
